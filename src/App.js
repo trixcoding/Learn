8955cf94d4345dd3bb9aed1cd15031c0e33d4989
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-04 16:14:08 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Edited in github
         </a>
       </header>
     </div>
