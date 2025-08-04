295f4ea8d84c89b83e2be7d7c2fc9a93e88ea8f3
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-08-04 16:26:38 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Edited in git
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Edited in github
-        </a>
-      </header>
+    <div>
+      <h1>سلام React!</h1>
+      <p>این اولین کامپوننت من است.</p>
     </div>
   );
 }
